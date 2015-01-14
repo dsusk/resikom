cfdf34a960fca2d24df6d982e3849703ed796b90
dsusk <tech@sub.uni-goettingen.de>
2015-01-14 14:35:27 +0100

    added perl script for geopoint-index

---
+++ perl/geoPointsHBI.pl
@@ -0,0 +1,52 @@
+#!/usr/bin/perl
+
+my $USAGE="geoPoints.pl <XML-DB-File>";
+
+if(@ARGV!=1){print $USAGE; exit -1}
+open db_gp,  $ARGV[0] || die "cannot open ".$ARGV[0]."\n";
+
+
+my @points=<db_gp>;
+chomp@points;
+
+
+# Writing the index
+my $op='<field name="';
+my $cls='">';
+my $endtag='</field>';
+
+print '<?xml version="1.0" encoding="UTF-8"?><add>';
+for(my $i=0;$i<@points;$i++){
+	if($points[$i]=~/\<field name=\"ID\"\>/){
+		my $ID=&trimField($points[$i], "ID");
+		my $articleID=&trimField($points[$i+1], "HB1ID");
+		my $latitude=&trimField($points[$i+2], "lat");
+		my $longitude=&trimField($points[$i+3], "lon");
+			
+			print "\n";
+			print'<doc>';
+			print $op."ID".$cls."geoPointHB1-".$ID.$endtag;		
+			print $op."articleID".$cls.$articleID.$endtag;
+			print $op."dok-name".$cls."rf15_I_121220".$endtag;		
+			print $op."type".$cls."geopoint".$endtag;		
+			print $op."geoPointLatitude".$cls.$latitude.$endtag;		
+			print $op."geoPointLongitude".$cls.$longitude.$endtag;		
+			print'</doc>';
+	}
+}
+print"</add>";
+
+
+sub trimField{
+	$line=shift(@_);
+	$fieldname=shift(@_);
+	if($line=~/\<\/field\>/){
+	#$line=~s/\s*xsi:nil=\"true\"\s*//;
+	$line=~s/\<field name=\"$fieldname\"\s*\>//;
+        $line=~s/\<\/field\>//;
+	$line=~s/\s*//;
+	}else{ $line =''; }
+	return $line;		
+}
+
+
